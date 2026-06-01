12e9b905c050464329b63543bfce5755d0760230
Mohit <119338819+MohitAr2@users.noreply.github.com>
2026-06-01 13:09:18 +0530

    Update EntityMapper.java

--- backend/src/main/java/com/example/claimsbackend/mapper/EntityMapper.java
+++ backend/src/main/java/com/example/claimsbackend/mapper/EntityMapper.java
@@ -1,5 +1,7 @@
 package com.example.claimsbackend.mapper;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface EntityMapper<ENTITY, REQUESTDTO, RESPONSEDTO> {
     public ENTITY mapToEntity(REQUESTDTO requestdto);
     public RESPONSEDTO mapToResponse(ENTITY entity);
